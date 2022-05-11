47a42d752ceebb7bb6517589ac7040ad885668c6
Daniele Conti <fourlastor@users.noreply.github.com>
2022-05-11 19:46:07 +0100

    TODO: continue system test screen

---
+++ core/src/main/kotlin/io/github/fourlastor/monster/SystemTestScreen.kt
@@ -0,0 +1,110 @@
+package io.github.fourlastor.monster
+
+import com.artemis.World
+import com.artemis.WorldConfigurationBuilder
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
+import com.badlogic.gdx.InputAdapter
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.PerspectiveCamera
+import com.badlogic.gdx.math.Vector3
+import com.badlogic.gdx.utils.IntIntMap
+import io.github.fourlastor.monster.system.LightComponent
+import io.github.fourlastor.monster.system.RenderSystem
+import io.github.fourlastor.monster.system.SceneComponent
+import ktx.app.KtxScreen
+import net.mgsx.gltf.loaders.glb.GLBLoader
+import net.mgsx.gltf.scene3d.lights.DirectionalLightEx
+import net.mgsx.gltf.scene3d.scene.Scene
+import net.mgsx.gltf.scene3d.scene.SceneAsset
+import net.mgsx.gltf.scene3d.scene.SceneManager
+
+class SystemTestScreen : KtxScreen {
+
+    private val sceneAsset: SceneAsset = GLBLoader().load(Gdx.files.internal("../assets/character.glb"))
+
+    private val sceneManager: SceneManager = SceneManager()
+    private val world = WorldConfigurationBuilder()
+        .with(RenderSystem(sceneManager))
+        .build()
+        .let { World(it) }
+
+    private val camera: PerspectiveCamera =
+        PerspectiveCamera(60f, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat()).apply {
+            translate(Vector3(2f, 2f, -3f))
+            lookAt(Vector3.Zero)
+        }
+
+    private val cameraController = object : InputAdapter() {
+        private val keys = IntIntMap()
+
+        override fun keyDown(keycode: Int): Boolean {
+            keys.put(keycode, keycode)
+            return true
+        }
+
+        override fun keyUp(keycode: Int): Boolean {
+            keys.remove(keycode, 0)
+            return true
+        }
+
+        fun update(delta: Float) {
+            val rotation = delta * 100
+            when {
+                keys.containsKey(Input.Keys.A) -> {
+                    camera.rotateAround(Vector3.Y, Vector3.Y, rotation)
+                    camera.lookAt(Vector3.Zero)
+                    camera.update(true)
+                }
+                keys.containsKey(Input.Keys.D) -> {
+                    camera.rotateAround(Vector3.Y, Vector3.Y, -rotation)
+                    camera.lookAt(Vector3.Zero)
+                    camera.update(true)
+                }
+            }
+        }
+    }
+
+    init {
+        world.create()
+            .also {
+                world.edit(it)
+                    .create(SceneComponent::class.java).apply {
+                        scene = Scene(sceneAsset.scene).apply {
+                            animationController.animate("idle", -1, 1f, null, 0f)
+                        }
+                    }
+            }
+        world.create()
+            .also {
+                world.edit(it)
+                    .create(LightComponent::class.java).apply {
+                        light = DirectionalLightEx().apply {
+                            direction.set(1f, -3f, 1f).nor()
+                            color.set(Color.WHITE)
+                        }
+                    }
+            }
+        sceneManager.setCamera(camera)
+        sceneManager.setAmbientLight(0.2f)
+    }
+
+    override fun show() {
+        Gdx.input.inputProcessor = cameraController
+    }
+
+    override fun resize(width: Int, height: Int) {
+        sceneManager.updateViewport(width.toFloat(), height.toFloat())
+    }
+
+    override fun render(delta: Float) {
+        world.setDelta(delta)
+        world.process()
+        cameraController.update(delta)
+    }
+
+    override fun dispose() {
+        sceneAsset.dispose()
+        world.dispose()
+    }
+}
---
+++ core/src/main/kotlin/io/github/fourlastor/monster/system/RenderSystem.kt
@@ -0,0 +1,57 @@
+package io.github.fourlastor.monster.system
+
+import com.artemis.BaseEntitySystem
+import com.artemis.ComponentMapper
+import com.artemis.PooledComponent
+import com.artemis.annotations.One
+import com.badlogic.gdx.graphics.g3d.environment.BaseLight
+import io.github.fourlastor.monster.extension.onMapper
+import net.mgsx.gltf.scene3d.scene.Scene
+import net.mgsx.gltf.scene3d.scene.SceneManager
+
+@One(SceneComponent::class, LightComponent::class)
+class RenderSystem(
+    private val manager: SceneManager,
+) : BaseEntitySystem() {
+
+    private lateinit var sceneMapper: ComponentMapper<SceneComponent>
+    private lateinit var lightMapper: ComponentMapper<LightComponent>
+
+    override fun processSystem() {
+        manager.update(world.delta)
+        manager.render()
+    }
+
+    override fun dispose() {
+        manager.dispose()
+    }
+
+    override fun inserted(entityId: Int) {
+        entityId.onMapper(sceneMapper) { manager.addScene(it.scene) }
+        entityId.onMapper(lightMapper) { manager.environment.add(it.light) }
+    }
+
+    override fun removed(entityId: Int) {
+        entityId.onMapper(sceneMapper) { manager.removeScene(it.scene) }
+        entityId.onMapper(lightMapper) { manager.environment.remove(it.light) }
+    }
+
+}
+
+
+class SceneComponent(
+    var scene: Scene? = null
+) : PooledComponent() {
+
+    override fun reset() {
+        scene = null
+    }
+}
+
+class LightComponent(
+    var light: BaseLight<*>? = null
+) : PooledComponent() {
+    override fun reset() {
+        light = null
+    }
+}
